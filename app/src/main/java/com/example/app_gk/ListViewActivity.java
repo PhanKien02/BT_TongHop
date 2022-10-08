e676722032ba0cdec744368ca7b5877bd0b10f63
Phan Kien <phankien110302@gmil.com>
2022-10-08 22:58:53 +0700

    description fruit

--- app/src/main/java/com/example/app_gk/ListViewActivity.java
+++ app/src/main/java/com/example/app_gk/ListViewActivity.java
@@ -35,11 +35,20 @@ public class ListViewActivity extends AppCompatActivity {
 
       lv_fruit =  findViewById(R.id.list_view);
         fruits= new ArrayList<>();
-        fruits.add(new Fruit("Dâu tây","description dâu tây",R.drawable.dautay));
-        fruits.add(new Fruit("Nho", "description nho",R.drawable.nho));
-        fruits.add(new Fruit("Thơm", "description thơm",R.drawable.dua));
-        fruits.add(new Fruit("Chuoi", "description chuoi",R.drawable.chuoi));
-        fruits.add(new Fruit("Cherry", "description cherry",R.drawable.cherry));
+
+        fruits.add(new Fruit("Cherry", "Cherry là loại quả giàu dinh dưỡng giúp làm" +
+                " đẹp da và chế biến được đa dạng các món sinh tố, thức uống,...",R.drawable.cherry));
+        fruits.add(new Fruit("Nho", "Nho là một loại quả mọng lấy từ các loài cây thân leo thuộc" +
+                " chi Nho (Vitis). Quả nho mọc thành chùm từ 6 đến 300 quả, chúng có màu đen, " +
+                "lam, vàng, lục, đỏ-tía hay trắng...",R.drawable.nho));
+        fruits.add(new Fruit("Chuoi", "Chuối là tên gọi loại quả của các loài cây thuộc chi Musa;đây được coi là một trong các loại trái cây được ăn rộng rãi nhất. Những cây chuối " +
+                "có gốc từ vùng nhiệt đới ở Đông Nam Á..." +
+                " vùng nhiệt đới...",R.drawable.chuoi));
+        fruits.add(new Fruit("Dâu tây"," Dâu tây là một chi thực vật hạt kín và loài thực vật có hoa thuộc họ " +
+                "Hoa hồng (Rosaceae). Dâu tây xuất xứ từ châu Mỹ và được các nhà làm vườn châu Âu... ",R.drawable.dautay));
+        fruits.add(new Fruit("Thơm", "Dứa có các tên gọi khác như là: khóm, thơm, khớm, gai hoặc huyền nương, tên khoa học Ananas comosus," +
+                " là một loại quả nhiệt đới...",R.drawable.dua));
+
         adapter = new Adapter(ListViewActivity.this,R.layout.activity_item,fruits);
         lv_fruit.setAdapter(adapter);
         lv_fruit.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
